fb4c3f507c96b07cffd4da3b8fdcefb959cd7fac
workbytaylor <79762858+workbytaylor@users.noreply.github.com>
2022-11-13 23:58:26 -0500

    Initial Commit

---
+++ Interval Watch App/IntervalApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalApp.swift
+//  Interval Watch App
+//
+//  Created by Nilakshi Roy on 2022-11-13.
+//
+
+import SwiftUI
+
+@main
+struct Interval_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Interval/IntervalApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalApp.swift
+//  Interval
+//
+//  Created by Nilakshi Roy on 2022-11-13.
+//
+
+import SwiftUI
+
+@main
+struct IntervalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
