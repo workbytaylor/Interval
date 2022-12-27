532151887e1b8edff7786a39776a23782068b1a6
workbytaylor <79762858+workbytaylor@users.noreply.github.com>
2022-12-27 08:37:11 -0500

    Watch Start
    Began playing around with watch views.

--- Interval Watch App/ContentView.swift
+++ Interval Watch App/ContentView.swift
@@ -9,18 +9,68 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        
+        
+        
+        List {
+            
+            Section {
+                VStack(alignment: .leading) {
+                    // key info on run
+                    
+                    Text("40' Marathon Pace")
+                        .font(.title3)
+                    Text("8km | 38m")
+                        .font(.caption2)
+                        .foregroundStyle(.secondary)
+                }
+                
+                
+            }
+            
+ 
+            Section(header: Text("Steps")) {
+                
+                ForEach(1..<5) { workout in
+                    NavigationLink {
+                        //
+                    } label: {
+                        HStack {
+                            Image(systemName: "stopwatch")
+                            Text("800m")
+                        }
+                        .foregroundStyle(.secondary)
+                    }
+                }
+                Button {
+                    // start run
+                } label: {
+                    Text("Start")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                }
+                .listItemTint(.yellow)
+            }
+            
         }
-        .padding()
+        .listStyle(.plain)
+        .navigationTitle {
+            HStack {
+                Text("Today's Run")
+                Spacer()
+            }
+            
+            
+        }   //Strava uses this here
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationStack {
+            ContentView()
+        }
     }
 }
