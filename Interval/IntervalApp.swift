f7c0296d63ec0cb0c22d7bdfb64ff9c0a135c727
workbytaylor <79762858+workbytaylor@users.noreply.github.com>
2023-03-12 08:36:49 -0400

    Commit

--- Interval/IntervalApp.swift
+++ Interval/IntervalApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct IntervalApp: App {
     
-    @StateObject private var dataController = DataController()
+    @StateObject var dataController = DataController()
     
     var body: some Scene {
         WindowGroup {
