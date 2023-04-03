7d369fcfd492c2be57baecc7f1170cb7e7ca2420
workbytaylor <79762858+workbytaylor@users.noreply.github.com>
2023-04-03 06:10:58 -0400

    Picker changes

--- Interval/Views/Create/Pickers/TimePicker.swift
+++ Interval/Views/Create/Pickers/TimePicker.swift
@@ -12,29 +12,37 @@ struct TimePicker: View {
     @Binding var step: Step
     var timeRange = Int16(1)...Int16(1000)
     var timeUnits: [String] = ["hours", "minutes", "seconds"]
-    
+    @State var magnitude: String = "0"
     
     var body: some View {
         HStack(spacing: .zero) {
+            /*
             Picker("Magnitude", selection: $step.magnitude) {
                 ForEach(timeRange, id: \.self) { magnitude in
                     Text(String(magnitude))
                 }
             }
-            
             .pickerStyle(.wheel)
+            */
+            Text("Length")
+            Spacer()
+            TextField("Magnitude", text: $magnitude)
+                .frame(width: 100)
+                .keyboardType(.numberPad)   // is this the right keyboard?
+            
             Picker("Unit", selection: $step.unit) {
                 ForEach(timeUnits, id: \.self) { unit in
-                    Text(unit)
+                    Text(unit.capitalized)
                 }
             }
-            .pickerStyle(.wheel)
+            .labelsHidden()
+            //.pickerStyle(.wheel)
         }
     }
 }
 
 struct TimePicker_Previews: PreviewProvider {
     static var previews: some View {
-        TimePicker(step: .constant(Step()))
+        TimePicker(step: .constant(Step()), magnitude: "0")
     }
 }
